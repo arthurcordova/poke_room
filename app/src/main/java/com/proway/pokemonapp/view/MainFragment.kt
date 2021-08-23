bb624ccaace55ccfef9fd3aea4a48007ca833d03
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2021-08-23 11:06:09 -0300

    view model + repository

--- app/src/main/java/com/proway/pokemonapp/view/MainFragment.kt
+++ app/src/main/java/com/proway/pokemonapp/view/MainFragment.kt
@@ -7,27 +7,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.proway.pokemonapp.R
+import com.proway.pokemonapp.databinding.MainFragmentBinding
 import com.proway.pokemonapp.view_model.MainViewModel
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = MainFragmentBinding.bind(view)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.fetchAllFromServer()
+
     }
 
 }
